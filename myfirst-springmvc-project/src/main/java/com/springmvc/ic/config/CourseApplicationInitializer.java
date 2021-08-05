59ddd4b02b9fc013759c31b4e507d1fbba29d328
001ZBA744 <001ZBA744@WIN-K3N4KS1NBUK>
2021-08-06 00:16:02 +0530

    Added code

---
+++ myfirst-springmvc-project/src/main/java/com/springmvc/ic/config/CourseApplicationInitializer.java
@@ -0,0 +1,30 @@
+package com.springmvc.ic.config;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.context.support.XmlWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+public class CourseApplicationInitializer implements WebApplicationInitializer {
+
+	public void onStartup(ServletContext servletContext) throws ServletException {
+		
+//		XmlWebApplicationContext webApplicationContext= new XmlWebApplicationContext();
+//		webApplicationContext.setConfigLocation("classpath:beans.xml");
+		
+		AnnotationConfigWebApplicationContext webApplicationContext=new AnnotationConfigWebApplicationContext();
+		webApplicationContext.register(CourseAppConfig.class);
+		
+		DispatcherServlet dispatcherServlet= new DispatcherServlet(webApplicationContext);
+		
+		ServletRegistration.Dynamic myCustomServletDispatcherServlet= servletContext.addServlet("myDispatcherServlet", dispatcherServlet);
+		
+		myCustomServletDispatcherServlet.setLoadOnStartup(1);
+		myCustomServletDispatcherServlet.addMapping("/mywebsite.com/*");
+	}
+
+}
---
+++ myfirst-springmvc-project/src/main/webapp/WEB-INF/view/helloworld.jsp
@@ -0,0 +1,11 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+
+<title>first spring mvc app</title>
+</head>
+<body>
+<h5>Welcome</h5>
+</body>
+</html>
